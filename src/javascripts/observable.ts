e14b84f84060b6c69e19ebc3cf1d47cd4c3a7330
kxrn0 <kxrp@protonmail.com>
2024-01-05 08:28:16 -0600

    added a delay to wait for stuff to load before observing

--- src/javascripts/observable.ts
+++ src/javascripts/observable.ts
@@ -1,15 +1,19 @@
 const observables = document.querySelectorAll(".observable");
 
-observables.forEach((item) => {
-  const observer = new IntersectionObserver(
-    (entries) => {
-      const entry = entries[0];
+setTimeout(
+  () =>
+    observables.forEach((item) => {
+      const observer = new IntersectionObserver(
+        (entries) => {
+          const entry = entries[0];
 
-      if (entry.isIntersecting) entry.target.classList.add("observed");
-      // else entry.target.classList.remove("observed");
-    },
-    { threshold: 1 }
-  );
+          if (entry.isIntersecting) entry.target.classList.add("observed");
+          // else entry.target.classList.remove("observed");
+        },
+        { threshold: 1 }
+      );
 
-  observer.observe(item);
-});
+      observer.observe(item);
+    }),
+  1000
+);
